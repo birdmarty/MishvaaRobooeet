da0de48439b21689152c74f47f23c48610c1cef1
DESKTOP-76EP6K3 <newpcnostig@gmail.com>
2024-04-29 01:55:36 +0300

    final working quadratic calculator

--- app/src/main/java/com/example/mishvaarobooeet/MainActivity.java
+++ app/src/main/java/com/example/mishvaarobooeet/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.mishvaarobooeet;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -17,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText InputA;
     private EditText InputB;
     private EditText InputC;
+    private TextView ans;
+    private final int REQUEST_CODE=100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +31,38 @@ public class MainActivity extends AppCompatActivity {
         InputA = findViewById(R.id.InputA);
         InputB = findViewById(R.id.InputB);
         InputC = findViewById(R.id.InputC);
+        ans = findViewById(R.id.tv);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==REQUEST_CODE){
+            if(resultCode== Activity.RESULT_OK){
+                if(data!=null){
+                    double a = data.getDoubleExtra("numA", 0);
+                    double discriminant = data.getDoubleExtra("discriminant", 0);
+                    if (a == 0 || discriminant < 0 ){
+                        ans.setText("no solution");
+                    }
+                    else{
+                        double root1 = data.getDoubleExtra("root1", 0);
+                        double root2 = data.getDoubleExtra("root2", 0);
+                        ans.setText("x1 = "  + root1 + "\nx2 = " + root2);
+                    }
+                }
+                else {
+                    Log.i("MainActivity","NO data returned");
+                }
+            }
+            else {
+                Log.i("MainActivity","Bad return code");
+            }
+        }
     }
     public static int get_rand() {
         Random rand = new Random();
-        return (int) Math.floor(Math.random() * (89) + 10);
+        return (int) Math.floor(Math.random() * (401) - 200);
     }
     public void rng(View view) {
         numA = get_rand();
@@ -49,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         si.putExtra("numB", numB);
         si.putExtra("numC", numC);
 
-        startActivity(si);
+        startActivityForResult(si, REQUEST_CODE);
     }
 
 }
--- app/src/main/java/com/example/mishvaarobooeet/MainActivity2.java
+++ app/src/main/java/com/example/mishvaarobooeet/MainActivity2.java
@@ -1,20 +1,18 @@
 package com.example.mishvaarobooeet;
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
     private TextView ans;
-    private double numA;
-    private double numB;
-    private double numC;
-    private double root1;
-    private double root2;
-    private double ansblud;
+    private double numA, numB, numC, discriminant, division, root1, root2;
+    private ImageView parabola;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -24,15 +22,16 @@ public class MainActivity2 extends AppCompatActivity {
 
         // Initialize ans TextView
         ans = findViewById(R.id.Answer);
+        parabola= findViewById(R.id.parabola);
 
         Intent intent = getIntent();
-        if(intent != null) {
+        if (intent != null) {
             numA = intent.getDoubleExtra("numA", 0);
             numB = intent.getDoubleExtra("numB", 0);
             numC = intent.getDoubleExtra("numC", 0);
-
-            // Calculate the discriminant
-            double discriminant = numB * numB - 4 * numA * numC;
+            // Calculate the discriminant and the divider
+            discriminant = (numB * numB) - (4 * numA * numC);
+            division = 2 * numA;
 
             // Prepare the quadratic formula string with numbers
             String formula = String.format("x = (-%.2f ± √(%.2f² - 4*%.2f*%.2f)) / (2*%.2f)", numB, numB, numA, numC, numA);
@@ -40,35 +39,56 @@ public class MainActivity2 extends AppCompatActivity {
             // Display the formula
             ans.setText("Quadratic Formula:\n" + formula + "\n\n");
 
+            // Check if we are dividing by 0 ie - no solution
+            if (division == 0){
+                ans.append("\nNo soluton found: Dividng by 0");
+            }
+            else if (discriminant==0 && numA>0){
+                parabola.setImageResource(R.drawable.smileyparabola);
+            }
+            else if (discriminant>0 && numA>0){
+                parabola.setImageResource(R.drawable.fallingsmileyparabola);
+            }
+            else if (discriminant<0 && numA>0){
+                parabola.setImageResource(R.drawable.flyingsmileyparabola);
+            }
+            else if (discriminant==0 && numA<0){
+                parabola.setImageResource(R.drawable.sadparabola);
+            }
+            else if (discriminant>0 && numA<0){
+                parabola.setImageResource(R.drawable.fallingsadparabola);
+            }
+            else if (discriminant<0 && numA<0){
+                parabola.setImageResource(R.drawable.flyingsadparabola);
+            }
             // Calculate the roots using the quadratic formula
-            if (discriminant > 0) {
+            if (discriminant > 0 && division != 0) {
                 // Two real and distinct roots
-                root1 = (-numB + Math.sqrt(discriminant)) / (2 * numA);
-                root2 = (-numB - Math.sqrt(discriminant)) / (2 * numA);
-                ans.append("Roots:\n" + "x1 = " + root1 + "\nx2 = " + root2);
-            } else if (discriminant == 0) {
+                root1 = ((0-numB) + (Math.sqrt(discriminant))) / (2 * numA);
+                root2 = ((0-numB) - (Math.sqrt(discriminant))) / (2 * numA);
+                ans.append("\nx1 = " + root1 + "\nx2 = " + root2);
+            } else if (discriminant == 0 && division != 0) {
                 // One real root (repeated)
                 double root = -numB / (2 * numA);
                 ans.append("Root:\n" + "x = " + root);
-            } else {
-                // Complex roots
-                double realPart = -numB / (2 * numA);
-                double imaginaryPart = Math.sqrt(-discriminant) / (2 * numA);
-                ans.append("Roots:\n"
-                        + "x1 = " + realPart + " + " + imaginaryPart + "i\n"
-                        + "x2 = " + realPart + " - " + imaginaryPart + "i");
             }
+            else {
+                ans.append("\nNo soluton found: Discriminant is numA negative number");
+            }
+
         }
     }
 
     public void goback(View view) {
-        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
-        intent.putExtra("root1", root1); // Assuming root1 and root2 are accessible here
-        intent.putExtra("root2", root2);
-        intent.putExtra("numA", numA);
-        intent.putExtra("numB", numB);
-        intent.putExtra("numC", numC);
-        startActivity(intent);
+        Intent ti = new Intent(MainActivity2.this, MainActivity.class);
+        ti.putExtra("root1", root1);
+        ti.putExtra("root2", root2);
+        ti.putExtra("discriminant", discriminant);
+        ti.putExtra("numA", numA);
+        ti.putExtra("numB", numB);
+        ti.putExtra("numC", numC);
+        setResult(Activity.RESULT_OK,ti);
+        finish();
     }
 }
 
